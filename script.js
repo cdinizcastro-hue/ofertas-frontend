ddcb6d1447c99c6ccf3cfd5a3f6aebce1ff8501a
Caio <cdinizcastro@gmail.com>
2026-01-08 13:20:30 -0300

    Corrige exibição das imagens dos destinos

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ async function carregarOfertas() {
   const container = document.getElementById("ofertas");
   container.innerHTML = "";
 
-  ofertas.forEach(o => {
-    const imagemDestino = `https://source.unsplash.com/featured/?${encodeURIComponent(o.destino)}`;
+  ofertas.forEach((o, i) => {
+    const imagem = `https://picsum.photos/600/400?random=${i + 1}`;
 
     const card = document.createElement("div");
     card.className = "card";
 
     card.innerHTML = `
-      <div class="imagem-destino" style="background-image: url('${imagemDestino}')"></div>
+      <div class="imagem-destino">
+        <img src="${imagem}" alt="${o.destino}">
+      </div>
 
       <div class="conteudo-card">
         <h2>${o.origem} → ${o.destino}</h2>
         <div class="empresa">${o.empresa}</div>
 
-        <div class="precos">
-          <div class="preco-antigo">De R$ ${o.precoAntigo}</div>
-          <div class="preco-novo">Por R$ ${o.precoAtual}</div>
-        </div>
+        <div class="preco-antigo">De R$ ${o.precoAntigo}</div>
+        <div class="preco-novo">Por R$ ${o.precoAtual}</div>
 
         <a href="${o.link}" target="_blank" class="btn-oferta">
           Ver oferta
